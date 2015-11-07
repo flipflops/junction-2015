dd728daa88acf05788a8303c6779763ebd380911
Sten Hägglund <sten.hagglund@gmail.com>
2015-11-07 12:58:42 +0200

    Test button integration

--- src/server.js
+++ src/server.js
@@ -3,7 +3,16 @@ import express from 'express';
 
 const app = express();
 
+const state = {};
+
 app.get('/', (req, res)=> res.send('Hello World!'));
+
+app.get('/api/btn', (req, res)=> res.send('touched: ' + state.touched));
+
+app.post('/api/btn', (req, res)=> {
+  state.touched = new Date().getTime();
+  res.send(200);
+});
 
 const server = app.listen(process.env.PORT || 3000, ()=> {
   const host = server.address().host;
